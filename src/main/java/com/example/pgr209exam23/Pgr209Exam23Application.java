2a95de0e5014e10e616aeb4e1cda602e9b6785cd
danaithb <daha025@student.kristiania.no>
2023-12-06 17:58:53 +0100

    initial commit

---
+++ src/main/java/com/example/pgr209exam23/Pgr209Exam23Application.java
@@ -0,0 +1,13 @@
+package com.example.pgr209exam23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pgr209Exam23Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pgr209Exam23Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pgr209exam23/Pgr209Exam23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pgr209exam23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pgr209Exam23ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
