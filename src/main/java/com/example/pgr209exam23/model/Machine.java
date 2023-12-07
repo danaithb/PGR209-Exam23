6a109d8e3b7d91d0ec20624e32634c3b73eef55b
annarandeberg <anna-randeberg@hotmail.com>
2023-12-07 16:39:01 +0100

    Added the entities in the db(model class), but commented out, need to add som more db functionality first

--- src/main/java/com/example/pgr209exam23/model/Machine.java
+++ src/main/java/com/example/pgr209exam23/model/Machine.java
@@ -1,4 +1,44 @@
 package com.example.pgr209exam23.model;
+/*
+//Kilde: Vet clinic eksempel til læreren
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Machine {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "machine_seq_gen")
+    @SequenceGenerator(name = "machine_seq_gen", sequenceName = "machine_seq", allocationSize = 1)
+    @Column(name = "machine_id")
+    private Long machineId;
+
+    @Column(name = "machine_name")
+    private String machineName;
+
+    @Column(name = "machine_type")
+    private String machineType;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @OneToMany(mappedBy = "machine", cascade = CascadeType.ALL)
+    private List<Subassembly> subassemblies = new ArrayList<>();
+
+    public Machine(String machineName, String machineType, Order order) {
+        this.machineName = machineName;
+        this.machineType = machineType;
+        this.order = order;
+    }
 }
+*/
--- src/main/java/com/example/pgr209exam23/model/Part.java
+++ src/main/java/com/example/pgr209exam23/model/Part.java
@@ -1,4 +1,37 @@
 package com.example.pgr209exam23.model;
 
+//Kilde: Vet clinic eksempel til læreren
+/*
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Part {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "part_seq_gen")
+    @SequenceGenerator(name = "part_seq_gen", sequenceName = "part_seq", allocationSize = 1)
+    @Column(name = "part_id")
+    private Long partId;
+
+    @Column(name = "part_name")
+    private String partName;
+
+
+    @ManyToOne
+    @JoinColumn(name = "subassembly_id")
+    private Subassembly subassembly;
+
+    public Part(String partName, Subassembly subassembly) {
+        this.partName = partName;
+        this.subassembly = subassembly;
+    }
 }
+
+*/
