834964eec2984b972047217c6d92778d93803abb
danaithb <daha025@student.kristiania.no>
2023-12-10 21:33:42 +0100

    Added machine logic

---
+++ src/main/java/com/example/pgr209exam23/controller/MachineController.java
@@ -0,0 +1,49 @@
+package com.example.pgr209exam23.controller;
+
+import com.example.pgr209exam23.model.Machine;
+import com.example.pgr209exam23.service.MachineService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/machines")
+public class MachineController {
+
+    private final MachineService machineService;
+
+    @Autowired
+    public MachineController(MachineService machineService) {
+        this.machineService = machineService;
+    }
+
+    @GetMapping("/{id}")
+    public Machine getMachine(@PathVariable Long id) {
+        return machineService.findMachineById(id);
+    }
+
+    @PostMapping
+    public Machine createMachine(@RequestBody Machine machine) {
+        return machineService.createMachine(machine);
+    }
+
+    // Update
+    @PutMapping("/{id}")
+    public Machine updateMachine(@PathVariable Long id, @RequestBody Machine machine) {
+        return machineService.updateMachine(id, machine);
+    }
+
+    // Delete
+    @DeleteMapping("/{id}")
+    public void deleteMachine(@PathVariable Long id) {
+        machineService.deleteMachineById(id);
+    }
+
+    // Gets all
+    @GetMapping
+    public Page<Machine> getAllMachines(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        return machineService.getAllMachines(page, size);
+    }
+}
