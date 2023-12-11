92d0ec88e39b1256dd03e7072d0215bd6432884e
danaithb <daha025@student.kristiania.no>
2023-12-11 21:02:38 +0100

    Small change on tables

---
+++ src/main/resources/db.migration/V005_insert_factory.sql
@@ -0,0 +1,8 @@
+INSERT INTO customers (customer_name, customer_email) VALUES ('Ana', 'anna@example.com');
+
+INSERT INTO addresses (street, city, zip_code) VALUES ('Main Street 1', 'Annaville', '12345');
+INSERT INTO addresses (street, city, zip_code) VALUES ('Second Street 2', 'Annaville', '12345');
+
+-- Anta at vi får en ID på 1 og 2 for Annas adresser
+-- INSERT INTO customer_addresses (customer_id, address_id) VALUES (1, 1);
+--INSERT INTO customer_addresses (customer_id, address_id) VALUES (1, 2);
