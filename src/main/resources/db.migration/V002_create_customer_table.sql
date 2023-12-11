fae39757aa4a3dc93f6fdc149894d6f36ca38cdb
danaithb <daha025@student.kristiania.no>
2023-12-11 19:41:56 +0100

    Small change on tables

--- src/main/resources/db.migration/V002_create_customer_table.sql
+++
@@ -1,6 +0,0 @@
-create table customer(
-    customer_id bigint not null auto_increment primary key,
-    customer_name varchar(100) not null,
-    customer_email varchar(100) not null
-);
-
--- src/main/resources/db.migration/V003_create_order_table.sql
+++ src/main/resources/db.migration/V003_create_order_table.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS orders (
                                       order_id BIGINT AUTO_INCREMENT PRIMARY KEY,
                                       customer_id BIGINT NOT NULL,
                                       order_date TIMESTAMP NOT NULL,
-                                      FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
+                                      FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
