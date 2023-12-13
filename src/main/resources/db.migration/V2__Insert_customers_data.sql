66d6cf7451e79588a750f2f7f4ed5df2b7e82c41
danaithb <daha025@student.kristiania.no>
2023-12-13 17:36:16 +0100

    Added flyaway

---
+++ src/main/resources/db.migration/V2__Insert_customers_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO customers (customer_name, customer_email) VALUES ('Anna Smith', 'anna@example.com');
+INSERT INTO customers (customer_name, customer_email) VALUES ('John Doe', 'john.doe@example.com');
