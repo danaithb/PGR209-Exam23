0587a9bd7dcdc02990be6125d53d094d7a357621
danaithb <daha025@student.kristiania.no>
2023-12-13 19:07:41 +0100

    Added flyaway logic to insert customer (test ish)

--- src/main/resources/db.migration/V001__create_customer_table.sql
+++ src/main/resources/db.migration/V001__create_customer_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS customers (
+CREATE TABLE IF NOT EXISTS customer (
   customer_id BIGINT AUTO_INCREMENT PRIMARY KEY,
   customer_name VARCHAR(255) NOT NULL,
   customer_email VARCHAR(255) NOT NULL
--- src/main/resources/db.migration/V2__Insert_customers_data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO customers (customer_name, customer_email) VALUES ('Anna Smith', 'anna@example.com');
-INSERT INTO customers (customer_name, customer_email) VALUES ('John Doe', 'john.doe@example.com');
