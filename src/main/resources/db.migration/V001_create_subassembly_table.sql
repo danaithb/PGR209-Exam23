fd99d3264e7c5eee0ad3b5a80381b729c6e909b5
danaithb <daha025@student.kristiania.no>
2023-12-13 16:57:45 +0100

    Removed unecasrrye gaps

--- src/main/resources/db.migration/V001_create_subassembly_table.sql
+++ src/main/resources/db.migration/V001_create_subassembly_table.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS subassembly (
-                                           subassembly_id BIGINT NOT NULL PRIMARY KEY,
-                                           subassembly_name VARCHAR(255),
-                                           subassembly_article_number VARCHAR(255),
-                                           machine_id BIGINT,
-                                           CONSTRAINT fk_machine_subassembly FOREIGN KEY (machine_id) REFERENCES machines(machine_id) ON DELETE CASCADE
+    subassembly_name VARCHAR(255),
+    subassembly_article_number VARCHAR(255),
+    machine_id BIGINT,
+    CONSTRAINT fk_machine_subassembly FOREIGN KEY (machine_id) REFERENCES machines(machine_id) ON DELETE CASCADE
 );
