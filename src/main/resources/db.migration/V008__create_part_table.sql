1ba7e72be3fa6bb4011c70d852f6a6f5bbda33aa
danaithb <daha025@student.kristiania.no>
2023-12-14 17:47:15 +0100

    Removed uneccasrey spaces

--- src/main/resources/db.migration/V008__create_part_table.sql
+++ src/main/resources/db.migration/V008__create_part_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE if not exists part (
-                      part_id BIGINT PRIMARY KEY AUTO_INCREMENT,
-                      part_name VARCHAR(255) NOT NULL,
-                      subassembly_id BIGINT,
-                      FOREIGN KEY (subassembly_id) REFERENCES subassembly(subassembly_id)
+    part_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    part_name VARCHAR(255) NOT NULL,
+    subassembly_id BIGINT,
+    FOREIGN KEY (subassembly_id) REFERENCES subassembly(subassembly_id)
 );
