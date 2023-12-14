34b956289949da97ad5f7a350db1814ef738c64e
annarandeberg <anna-randeberg@hotmail.com>
2023-12-14 15:43:12 +0100

    added part

---
+++ src/main/java/com/example/pgr209exam23/controller/PartController.java
@@ -0,0 +1,53 @@
+package com.example.pgr209exam23.controller;
+
+import com.example.pgr209exam23.model.Customer;
+import com.example.pgr209exam23.model.Part;
+import com.example.pgr209exam23.service.PartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/part")
+public class PartController {
+
+    private final PartService partService;
+
+    @Autowired
+    public PartController(PartService partService) {
+        this.partService = partService;
+    }
+
+    @GetMapping("/{id}")
+    public Part getPart(@PathVariable Long id) {
+        return partService.findPartById(id);
+    }
+
+    /* @PostMapping
+     public ResponseEntity<Part> createPart(@RequestBody Part part) {
+         Part newPart = partService.createPart(part);
+         return new ResponseEntity<>(newPart, HttpStatus.CREATED);
+     }
+
+     @DeleteMapping("/{id}")
+     public void deletePart(@PathVariable Long id) {
+         partService.deletePartById(id);
+     }
+
+     @PutMapping
+     public Part updatePart(@RequestBody Part part) {
+         return partService.updatePart(part);
+     }
+ */
+    @GetMapping
+    public ResponseEntity<Page<Part>> getParts(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        Page<Part> parts = partService.getParts(page, size);
+        return new ResponseEntity<>(parts, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/example/pgr209exam23/model/Part.java
@@ -0,0 +1,35 @@
+package com.example.pgr209exam23.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Part {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "part_id")
+    private Long partId;
+
+    @Column(name = "part_name")
+    private String partName;
+
+
+    @ManyToOne
+    @JoinColumn(name = "subassembly_id")
+    @JsonIgnore
+    private Subassembly subassembly;
+
+    public Part(String partName) {
+        this.partName = partName;
+    }
+}
