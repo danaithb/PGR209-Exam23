84b802ac996f3ce3383e5bb320095b6702628782
danaithb <daha025@student.kristiania.no>
2023-12-14 18:13:25 +0100

    Insert subass

--- src/main/resources/db.migration/V007__Insert_subassembly_data.sql
+++ src/main/resources/db.migration/V007__Insert_subassembly_data.sql
@@ -1,3 +1,2 @@
 INSERT INTO subassembly (subassembly_name, subassembly_article_number, machine_id) VALUES ('Gear Box', 'GBX-342', 1);
 INSERT INTO subassembly (subassembly_name, subassembly_article_number, machine_id) VALUES ('Hydraulic Pump', 'HP-783', 2);
-
