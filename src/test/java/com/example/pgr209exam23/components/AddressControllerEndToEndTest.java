c1d8391047f17156325b500db82265e10ad00e20
annarandeberg <anna-randeberg@hotmail.com>
2023-12-17 20:57:26 +0100

    started on some tests for order and address

--- src/test/java/com/example/pgr209exam23/components/AddressControllerEndToEndTest.java
+++ src/test/java/com/example/pgr209exam23/components/AddressControllerEndToEndTest.java
@@ -1,4 +1,28 @@
 package com.example.pgr209exam23.components;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+//måtte bruke MockMvcRequestBuilders her, funket ikke med get og delete
+@SpringBootTest
+@AutoConfigureMockMvc
 public class AddressControllerEndToEndTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Test
+    void shouldDeleteAddress() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/address/{id}", 1))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/address/{id}", 1))
+                .andExpect(status().isNotFound());
+
+    }
+
 }
