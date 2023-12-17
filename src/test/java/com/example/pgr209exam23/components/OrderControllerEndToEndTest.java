77de85b08773ccd2dd0b57682396478f8b546d5d
annarandeberg <anna-randeberg@hotmail.com>
2023-12-18 00:28:38 +0100

    tests for order, address and part

--- src/test/java/com/example/pgr209exam23/components/OrderControllerEndToEndTest.java
+++ src/test/java/com/example/pgr209exam23/components/OrderControllerEndToEndTest.java
@@ -6,30 +6,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class PartControllerEndToEndTest {
+public class OrderControllerEndToEndTest {
 
     @Autowired
     private MockMvc mockMvc;
+
     @Test
-    void shouldFetchPartById() throws Exception {
-        mockMvc.perform(get("/api/part/{id}", 1))
+    void shouldFetchAllOrders() throws Exception{
+        mockMvc.perform(get("/api/order"))
                 .andExpect(status().isOk())
-                .andExpect(content().json("{\"partId\":1,\"partName\":\"Girkasse Del 1\"}"));
+                .andExpect(content().contentType("application/json"));
     }
 
-    //må sjekke opp i noe før jeg kan fikse denne
-    /*@Test
-    void shouldDeletePartById() throws Exception {
-        mockMvc.perform(delete("/api/part/{id}", 1))
-                .andExpect(status().isNoContent());
-    }*/
 
 }
